7cd738d41661f9a3be30dfc8c6b97ce5a72bb509
adamszegedi <mail@adamszegedi.eu>
2023-06-09 12:54:41 +0200

    hyprland config changed

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,18 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "javascript", "typescript", "rust", "vim", "c", "lua", "vim" },
+    ensure_installed = {
+        "javascript",
+        "typescript",
+        "rust",
+        "vim",
+        "c",
+        "lua",
+        "vim",
+        "css",
+        "html",
+        "json",
+        "yaml",
+    },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
