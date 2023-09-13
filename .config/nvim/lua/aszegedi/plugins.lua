efadfb0d8ee18c7b51ae5b53e6dcad7e7badb2bf
adamszegedi <mail@adamszegedi.eu>
2023-09-13 20:21:53 +0200

    Minor style changes, fonts mainly

--- .config/nvim/lua/aszegedi/plugins.lua
+++ .config/nvim/lua/aszegedi/plugins.lua
@@ -36,8 +36,14 @@ return {
         config = function()
             vim.o.timeout = true
             vim.o.timeoutlen = 300
-            require("which-key").setup()
-        end,
+            require("which-key").setup {
+                  icons = {
+                    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+                    separator = "»", -- symbol used between a key and it's label
+                    group = "+", -- symbol prepended to a group
+                },
+            }
+       end,
     },
     { 'sainnhe/gruvbox-material' },
     {
