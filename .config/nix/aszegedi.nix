b6454f9dc37f44fc8a9bf32181a38a13ef294fb1
adamszegedi <mail@adamszegedi.eu>
2024-08-18 21:27:40 +0200

    Cursor finally set

--- .config/nix/aszegedi.nix
+++ .config/nix/aszegedi.nix
@@ -1,17 +1,52 @@
 { config, lib, pkgs, ... }:
 
 {
+  imports = [ <home-manager/nixos> ];
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.aszegedi = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      neovim gnome3.gnome-tweaks mc yadm gh nvtopPackages.amd htop
-      git eza bat
-    ];
   };
 
-  fonts.packages = with pkgs; [ (nerdfonts.override { fonts = [ "JetBrainsMono" "SourceCodePro"]; }) ];
+  home-manager.users.aszegedi = { pkgs, ... }: {
+    home.stateVersion = "24.05";
+    home.packages = with pkgs; [
+      fastfetch neovim mc yadm gh nvtopPackages.amd htop
+      git eza bat xfce.thunar 
+    ];
+    #wayland.windowManager.hyprland.enable = true;
+
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        text-scaling-factor = 1.4;
+        cursor-theme = "Bibata-Modern-Ice";
+        cursor-size = 36;
+      };
+    };
+
+    gtk = {
+      enable = true;
+      theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome.gnome-themes-extra;
+      };
+      #gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+      #gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+      font = {
+          name = "DejaVu Sans";
+          size = 11;
+      };
+    };
+
+    qt = {
+      enable = true;
+      style = {
+          name = "adwaita-dark";
+      };
+    };
+  };
 
   fileSystems."/home/aszegedi/Games" = { device = "/dev/disk/by-uuid/db2640a7-927d-46b5-b790-9090d5a39c24";
       fsType = "btrfs";
--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -16,102 +16,52 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "archie"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Europe/Budapest";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = (with pkgs; [
-	  gnome-photos
-	  gnome-tour
-	  gnome-text-editor
-          gedit
-  ]) ++ (with pkgs.gnome; [
-	  cheese # webcam tool
-	  gnome-calendar
-	  gnome-contacts
-	  gnome-system-monitor
-	  gnome-maps
-	  gnome-music
-	  gnome-terminal
-	  epiphany # web browser
-	  geary # email reader
-	  evince # document viewer
-	  gnome-characters
-	  totem # video player
-	  tali # poker game
-	  iagno # go game
-	  hitori # sudoku game
-	  atomix # puzzle game
-  ]);
   
   # Flatpak
   services.flatpak.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  services.pipewire.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
 
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-     gnomeExtensions.appindicator
+     foot
+     grim
+     hyprland
+     hyprpaper
+     hypridle
+     hyprlock
+     hyprcursor
+     mako
+     slurp
+     tofi
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
+     waybar
+     wl-clipboard
+     xdg-desktop-portal-gtk
   ];
-  # gnome-settings-deamon udev rules
-  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+
+  environment.sessionVariables = {
+      QT_SCALE_FACTOR = "1.5";
+      QT_QPA_PLATFORMTHEME = "qt6ct";
+  };
+
+  fonts.packages = with pkgs; [ (nerdfonts.override { fonts = [ "JetBrainsMono" "SourceCodePro" "DroidSansMono" "FiraCode" ]; }) ];
 
   programs.gnupg.agent.enable = true;
+  programs.hyprland.enable = true;
+  programs.dconf.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
@@ -136,6 +86,5 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
 
