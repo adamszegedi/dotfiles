c06b3dde18c1890d7257b5532a1745fba0f6c7c3
adamszegedi <mail@adamszegedi.eu>
2026-05-07 09:25:07 +0200

    Removed redundant env setup, moved editor variable to environment.d

--- dot_bashrc.d/executable_environment.sh
+++
@@ -1,4 +0,0 @@
-# shellcheck shell=bash
-[[ -f /run/.toolboxenv ]] && return
-
-export EDITOR=nvim
--- dot_bashrc.d/executable_ssh-agent.sh
+++
@@ -1,4 +0,0 @@
-# shellcheck shell=bash
-[[ -f /run/.toolboxenv ]] && return
-
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
