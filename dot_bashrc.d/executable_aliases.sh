33f6ee4c156433a3c9bcbd037b0a0ae49f894eb7
adamszegedi <mail@adamszegedi.eu>
2026-06-22 14:12:43 +0200

    Update .chezmoiignore Update .bashrc.d/aliases.sh Change attributes of .config/homebrew/Brewfile

--- dot_bashrc.d/executable_aliases.sh
+++ dot_bashrc.d/executable_aliases.sh
@@ -32,7 +32,7 @@ alias ports='sudo ss -tulpn'
 
 # Add safety nets 
 # do not delete / or prompt if deleting more than 3 files at a time #
-alias rm='rm -I --preserve-root'
+alias rm='rm -I'
 
 # confirmation #
 alias mv='mv -i'
